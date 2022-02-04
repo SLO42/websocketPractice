58dac1d5eb861ce59eba9aac63d8d1dd7d01248d
SLO42 <saolivei@student.42.us.org>
2022-02-03 21:47:36 -0600

    updated websocket and should send data to clients now.

--- Client/src/websocket/feed.js
+++ Client/src/websocket/feed.js
@@ -5,12 +5,14 @@ export const WebSocketDemo = () => {
   //Public API that will echo messages sent to it back to the client
   const [socketUrl, setSocketUrl] = useState('ws://localhost:3030');
   const [messageHistory, setMessageHistory] = useState([]);
+  const [msg, setMsg] = useState("");
 
   const {
     sendMessage,
     lastMessage,
     readyState,
-  } = useWebSocket(socketUrl);
+    
+  } = useWebSocket(socketUrl, {reconnectInterval: 30000, reconnectAttempts: 500, retryOnError: true});
 
 
   useEffect(() => {
@@ -20,9 +22,22 @@ export const WebSocketDemo = () => {
   }, [lastMessage, setMessageHistory]);
 
   const handleClickSendMessage = () => {
-    sendMessage('Hello');
-    lastMessage = "Hello";
+    sendMessage(msg);
+    lastMessage = msg;
   };
+
+  const handleRefresh = () => {
+
+    setSocketUrl(`ws://localhost:3030`);
+  }
+
+  const handleChange = (event) => {
+      setMsg(event.target.value);
+  }
+
+  const handleClear = () => {
+      setMsg("");
+  }
 
   const connectionStatus = {
     [ReadyState.CONNECTING]: 'Connecting',
@@ -34,13 +49,22 @@ export const WebSocketDemo = () => {
 
   return (
     <div>
+        <div>
+            <button onClick={() => handleRefresh()} disabled={readyState === ReadyState.OPEN}> Refresh connection.</button>
+        </div>
+        <div>
+            <input type="text" name="msg" value={msg} onChange={e => handleChange(e)}/>
+        <button onClick={handleClear} >clear</button>
+        </div>
       <button
         onClick={handleClickSendMessage}
         disabled={readyState !== ReadyState.OPEN}
       >
-        Click Me to send 'Hello'
+        Send
       </button>
-      <span>The WebSocket is currently {connectionStatus}</span>
+      <div>
+        <span>The WebSocket is currently {connectionStatus}</span>
+      </div>
       {lastMessage ? <span>Last message: {lastMessage.data}</span> : null}
       <ul>
         {messageHistory
--- Server/src/server.js
+++ Server/src/server.js
@@ -10,6 +10,7 @@ const server = app.listen(port, () => {
     console.log(`app running on port ${port}`)
 })
 const eWS = expressWs(app, server);
+const wss = eWS.getWss();
 
 app.use( (req, res, next) => {
     if (req.ws){
@@ -23,6 +24,21 @@ app.use( (req, res, next) => {
 app.get('/', (req, res) => {
     console.log('get route', req.testing);
     res.json({data: chat});
+})
+
+
+wss.on("connection", (ws) => {
+    ws.on("message", (data) => {
+        wss.clients.forEach(client => {
+            if (client !== ws && client.readyState === ws.OPEN){
+                client.send(data);
+            }
+            else if (client === ws && client.readyState === ws.OPEN) {
+                client.send("we did it!");
+            }
+        })
+    })
+    chat.push(" Welcome to the party" );
 })
 
 app.ws('/', (ws, req) => {
