bcf8349d70f4e0087d47a731449aa8cfcadc2250
SLO42 <saolivei@student.42.us.org>
2022-02-07 12:18:17 -0600

    updated ws route to require user, changed chatHistory to more of a user list and should reflect that.

--- Server/src/server.js
+++ Server/src/server.js
@@ -6,16 +6,21 @@ const port = 3030;
 
 let chatHistory = [];
 
-const server = app.listen(port, () => {
-    console.log(`app running on port ${port}`)
-})
+if (!server) {
+    var server = app.listen(port, () => {
+        console.log(`app running on port ${port}`)
+    })
+}
+
+
 const eWS = expressWs(app, server);
 const wss = eWS.getWss();
 
 app.use( (req, res, next) => {
     if (req.ws){
-        console.log("middleware for ws");
+        console.log("middleware for ws",);
     }
+
     req.testing = 'testing';
     return next();
 })
@@ -25,22 +30,38 @@ app.get('/', (req, res) => {
     res.json({data: chat});
 })
 
+// pretty sure this on connection thing doesn't work as intended!
 
+app.ws('/ws/:user?', (ws, req) => {
+    const user = req.params.user.slice(1);
+    console.log(user)
+    if (user){
+        if (!(chatHistory.includes(user))){
+            chatHistory.push(user)
+            wss.clients.forEach(client => {
+                client.send(JSON.stringify({msg: `${user} Joined` }));
+            })
+        }
 
-app.ws('/', (ws, req) => {
-    ws.on('connection', stream => {
-        console.log("awoke");
-    });
-
-    ws.on('message', (data) => {
-        const jsData = JSON.parse(data);
-        chatHistory.push(jsData);
-        wss.clients.forEach(client => {
-            if (client.readyState === ws.OPEN){
-                if (data){
-                    client.send(data);
-                }
+        ws.on('close', () => {
+            if (chatHistory.includes(user)){
+                console.log(user);
+                chatHistory.splice(chatHistory.indexOf(user), 1);
+                console.log(chatHistory);
+                wss.clients.forEach(client => {
+                    client.send(JSON.stringify({msg: `${user} has left the chat`, userToRemove: user}));
+                })
             }
-        })
-    });
+        });
+
+        ws.on('message', (data) => {
+            wss.clients.forEach(client => {
+                if (client.readyState === ws.OPEN){
+                    if (data){
+                        client.send(data);
+                    }
+                }
+            })
+        });
+    }
 });
