d027c6f54e96f1251ea67e934ba532dac85b34eb
SLO42 <saolivei@student.42.us.org>
2022-02-10 11:29:56 -0600

    added ESLint to server to enforce coding styles

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import expressWs from 'express-ws';
+import express from "express";
+import expressWs from "express-ws";
 
 const app = express();
 const port = 3030;
@@ -7,57 +7,57 @@ const port = 3030;
 let userList = [];
 
 if (!server) {
-    var server = app.listen(port, () => {
-        console.log(`app running on port ${port}`)
-    })
+	var server = app.listen(port, () => {
+		console.log(`app running on port ${port}`);
+	});
 }
 
 const eWS = expressWs(app, server);
 const wss = eWS.getWss();
 
 app.use( (req, res, next) => {
-    if (req.ws){
-        console.log("middleware for ws",);
-    }
+	if (req.ws){
+		console.log("middleware for ws",);
+	}
 
-    req.testing = 'testing'; 
-    return next();
-})
+	req.testing = "testing"; 
+	return next();
+});
 
-app.get('/', (req, res) => {
-    console.log('get route', req.testing);
-    res.json({data: chat});
-})
+app.get("/", (req, res) => {
+	console.log("get route", req.testing);
+	res.json({data: userList});
+});
 
 // pretty sure this on connection thing doesn't work as intended!
 
-app.ws('/ws/:user?', (ws, req) => {
-    const user = req.params.user.slice(1);
-    if (user){
-        if (!(userList.includes(user))){
-            userList.push(user)
-            wss.clients.forEach(client => {
-                client.send(JSON.stringify({msg: `${user} Joined` }));
-            })
-        }
+app.ws("/ws/:user?", (ws, req) => {
+	const user = req.params.user.slice(1);
+	if (user){
+		if (!(userList.includes(user))){
+			userList.push(user);
+			wss.clients.forEach(client => {
+				client.send(JSON.stringify({msg: `${user} Joined` }));
+			});
+		}
 
-        ws.on('close', () => {
-            if (userList.includes(user)){
-                userList.splice(userList.indexOf(user), 1);
-                wss.clients.forEach(client => {
-                    client.send(JSON.stringify({msg: `${user} has left the chat`, userToRemove: user}));
-                })
-            }
-        });
+		ws.on("close", () => {
+			if (userList.includes(user)){
+				userList.splice(userList.indexOf(user), 1);
+				wss.clients.forEach(client => {
+					client.send(JSON.stringify({msg: `${user} has left the chat`, userToRemove: user}));
+				});
+			}
+		});
 
-        ws.on('message', (data) => {
-            wss.clients.forEach(client => {
-                if (client.readyState === ws.OPEN){
-                    if (data){
-                        client.send(data);
-                    }
-                }
-            })
-        });
-    }
+		ws.on("message", (data) => {
+			wss.clients.forEach(client => {
+				if (client.readyState === ws.OPEN){
+					if (data){
+						client.send(data);
+					}
+				}
+			});
+		});
+	}
 });
